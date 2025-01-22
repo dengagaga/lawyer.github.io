6f39b59d961caeef5c8803cd371b0bae07e86f27
hexplode13 <hexplode13@gmail.com>
2025-01-22 17:27:49 +0300

    first commit 6

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const sectionSixItem2 = document.querySelector('.section_six-item-2')
 const sectionSixItem = document.querySelectorAll('.section_six-item')
 // const section_six-right-title-3-none = document.querySelector('.section_six-right-title-3-none')
 
-sectionSixBtn.addEventListener('click', () => {
-    sectionSixRightTitle3.classList.toggle('section_six-right-title-3-none')
-    sectionSixRightTitle4.classList.toggle('section_six-right-title-3-none')
-    sectionSixItem2.classList.toggle('section_six-right-title-3-none')
-    sectionSixItem.forEach(item => {
-        item.classList.toggle('section_six-right-title-3-none')
-    })
-})
+// sectionSixBtn.addEventListener('click', () => {
+//     sectionSixRightTitle3.classList.toggle('section_six-right-title-3-none')
+//     sectionSixRightTitle4.classList.toggle('section_six-right-title-3-none')
+//     sectionSixItem2.classList.toggle('section_six-right-title-3-none')
+//     sectionSixItem.forEach(item => {
+//         item.classList.toggle('section_six-right-title-3-none')
+//     })
+// })
 
 
 document.addEventListener('DOMContentLoaded', function() {
